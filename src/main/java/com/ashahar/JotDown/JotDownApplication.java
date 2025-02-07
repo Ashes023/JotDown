b0145f44e17114a0e98fd1ab305779c9acdd7190
Ashes023! <ashaharyunuskhan@gmail.com>
2025-02-07 23:51:49 +0530

    First commit

---
+++ src/main/java/com/ashahar/JotDown/JotDownApplication.java
@@ -0,0 +1,13 @@
+package com.ashahar.JotDown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JotDownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JotDownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashahar/JotDown/JotDownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashahar.JotDown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JotDownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
